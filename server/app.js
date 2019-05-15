8b64299b3b58be02f8bedaeca7f09a579eae2619
liuxuan <liuxuan351@foxmail.com>
2019-05-15 22:48:37 +0800

    change

--- server/app.js
+++ server/app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
-<<<<<<< HEAD
 const mongoose = require('mongoose');
-=======
->>>>>>> 6467802c25456302c38afe0db7a898690e3a9c35
 const schema = require('./schema/schema');
 
 const app = express();
 
-<<<<<<< HEAD
-// connect to Mongodb Atlas
 mongoose.connect('mongodb+srv://leocoder:qq123456@graphql-demo-vv9as.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
   console.log('connected to database');
@@ -18,10 +13,6 @@ mongoose.connection.once('open', () => {
 app.use('/graphql', graphqlHttp({
   schema,
   graphiql: true
-=======
-app.use('/graphql', graphqlHttp({
-  schema
->>>>>>> 6467802c25456302c38afe0db7a898690e3a9c35
 }));
 
 app.listen(4000, () => {
