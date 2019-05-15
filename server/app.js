5a41b666ed749baa644ad3d9330d06092be03ed7
liuxuan <liuxuan@unicloud.com>
2019-05-15 20:27:28 +0800

    finish mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.listen(4000, () => {
+// connect to Mongodb Atlas
+mongoose.connect('mongodb+srv://leocoder:qq123456@graphql-demo-vv9as.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
+app.use('/graphql', graphqlHttp({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
 });
