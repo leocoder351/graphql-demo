d7542d97ff64e35de927181a5917844faa713566
liuxuan <liuxuan351@foxmail.com>
2019-05-16 00:00:46 +0800

    add client

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const schema = require('./schema/schema');
 
 const app = express();
 
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mongodb
 mongoose.connect('mongodb+srv://leocoder:qq123456@graphql-demo-vv9as.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
   console.log('connected to database');
